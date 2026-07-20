1d78155460f5103cfefc9684696662ab9583568f
nekena <nekenabrunella@gmail.com>
2026-07-20 12:46:38 +0300

    config du route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,10 +19,21 @@ $routes->post('/client/retrait', 'ClientController::retrait');
 $routes->post('/client/transfert', 'ClientController::transfert');
 
 
-$routes->get('/operateur', 'OperateurController::index');
+// $routes->get('/operateur', 'OperateurController::index');
 
+// $routes->post('/operateur/prefixe/ajouter', 'OperateurController::ajouterPrefixe');
+// $routes->get('/operateur/prefixe/supprimer/(:num)', 'OperateurController::supprimerPrefixe/$1');
+
+// $routes->post('/operateur/bareme/ajouter', 'OperateurController::ajouterBareme');
+// $routes->get('/operateur/bareme/supprimer/(:num)', 'OperateurController::supprimerBareme/$1');
+
+
+// Dashboard opérateur
+$routes->get('/operateur', 'OperateurController::index');
+$routes->get('/operateur/prefixes', 'OperateurController::prefixes');
 $routes->post('/operateur/prefixe/ajouter', 'OperateurController::ajouterPrefixe');
 $routes->get('/operateur/prefixe/supprimer/(:num)', 'OperateurController::supprimerPrefixe/$1');
-
+$routes->get('/operateur/baremes', 'OperateurController::baremes');
 $routes->post('/operateur/bareme/ajouter', 'OperateurController::ajouterBareme');
 $routes->get('/operateur/bareme/supprimer/(:num)', 'OperateurController::supprimerBareme/$1');
+$routes->get('/operateur/clients', 'OperateurController::clients');
