287b089e938c966fc139f01058cf17a2a9e5ce81
nekena <nekenabrunella@gmail.com>
2026-07-20 16:49:35 +0300

    ajustement des routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,34 +6,39 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+// Authentification & Dashboard Client
 $routes->get('/', 'ClientController::loginView');
-
 $routes->get('/client/login', 'ClientController::loginView');
 $routes->post('/client/login', 'ClientController::login');
 $routes->get('/client/logout', 'ClientController::logout');
-
 $routes->get('/client/dashboard', 'ClientController::dashboard');
 
+// Transactions Client
 $routes->post('/client/depot', 'ClientController::depot');
 $routes->post('/client/retrait', 'ClientController::retrait');
 $routes->post('/client/transfert', 'ClientController::transfert');
 
+// Transfert Multiple (V2)
+$routes->get('/client/transfert-multiple', 'ClientController::transfertMultiple');
+$routes->post('/client/transfert-multiple/envoyer', 'ClientController::envoyerTransfertMultiple');
 
-// $routes->get('/operateur', 'OperateurController::index');
-
-// $routes->post('/operateur/prefixe/ajouter', 'OperateurController::ajouterPrefixe');
-// $routes->get('/operateur/prefixe/supprimer/(:num)', 'OperateurController::supprimerPrefixe/$1');
-
-// $routes->post('/operateur/bareme/ajouter', 'OperateurController::ajouterBareme');
-// $routes->get('/operateur/bareme/supprimer/(:num)', 'OperateurController::supprimerBareme/$1');
-
-
-// Dashboard opérateur
+// Backoffice Opérateur / Admin
 $routes->get('/operateur', 'OperateurController::index');
+
+// Gestion des Préfixes
 $routes->get('/operateur/prefixes', 'OperateurController::prefixes');
 $routes->post('/operateur/prefixe/ajouter', 'OperateurController::ajouterPrefixe');
 $routes->get('/operateur/prefixe/supprimer/(:num)', 'OperateurController::supprimerPrefixe/$1');
+
+// Gestion des Barèmes de frais
 $routes->get('/operateur/baremes', 'OperateurController::baremes');
 $routes->post('/operateur/bareme/ajouter', 'OperateurController::ajouterBareme');
 $routes->get('/operateur/bareme/supprimer/(:num)', 'OperateurController::supprimerBareme/$1');
+
+// Liste des Clients
 $routes->get('/operateur/clients', 'OperateurController::clients');
+
+// Gestion des Autres Opérateurs (V2)
+$routes->get('/operateur/autres-operateurs', 'OperateurController::autresOperateurs');
+$routes->post('/operateur/autres-operateurs/ajouter', 'OperateurController::ajouterAutreOperateur');
+$routes->get('/operateur/autres-operateurs/supprimer/(:num)', 'OperateurController::supprimerAutreOperateur/$1');
