1c1278181997a0793041328ff78e4bc2343baa3d
nekena <nekenabrunella@gmail.com>
2026-07-20 09:19:19 +0300

    conf

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+
+// Routes Opérateur
+$routes->get('/operateur', 'OperateurController::index');
+$routes->post('/operateur/prefixe/ajouter', 'OperateurController::ajouterPrefixe');
+$routes->get('/operateur/prefixe/supprimer/(:num)', 'OperateurController::supprimerPrefixe/$1');
+
+$routes->post('/operateur/bareme/ajouter', 'OperateurController::ajouterBareme');
+$routes->get('/operateur/bareme/supprimer/(:num)', 'OperateurController::supprimerBareme/$1');
